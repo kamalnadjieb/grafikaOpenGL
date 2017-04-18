764156dcac524fbdf5e90c036389f23e1203355a
Muhammad Kamal Nadjieb <13514054@std.stei.itb.ac.id>
2017-04-18 23:42:51 +0700

    add gunung dan background

--- pemandangan_windows/main.c
+++ pemandangan_windows/main.c
@@ -39,12 +39,12 @@ int WINAPI WinMain(HINSTANCE hInstance,
     /* create main window */
     hwnd = CreateWindowEx(0,
                           "GLSample",
-                          "OpenGL Sample",
+                          "Pemandangan",
                           WS_OVERLAPPEDWINDOW,
                           CW_USEDEFAULT,
                           CW_USEDEFAULT,
-                          256,
-                          256,
+                          500,
+                          500,
                           NULL,
                           NULL,
                           hInstance,
@@ -69,18 +69,41 @@ int WINAPI WinMain(HINSTANCE hInstance,
         } else {
             /* OpenGL animation code goes here */
 
-            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClearColor(0, 0, 0, 0);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glPushMatrix();
-            glRotatef(theta, 0.0f, 0.0f, 1.0f);
+            //glRotatef(theta, 0.0f, 0.0f, 1.0f);
 
+            // Gambar Langit
+            glBegin(GL_QUADS);
+                glColor3f(0, 0, 1);
+                glVertex2f(-1, 1);
+                glVertex2f(1, 1);
+
+                glColor3f(1, 1, 1);
+                glVertex2f(1, -1);
+                glVertex2f(-1, -1);
+            glEnd();
+
+            // Gambar Gunung 1
             glBegin(GL_TRIANGLES);
+                glColor3f(0, 1, 0);
+                glVertex2f(-1.25, -1);
+                glVertex2f(0.25, -1);
 
-                glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
-                glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
-                glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
+                glColor3f(1, 1, 1);
+                glVertex2f(-0.5, 0);
+            glEnd();
 
+            // Gambar Gunung 2
+            glBegin(GL_TRIANGLES);
+                glColor3f(0, 1, 0);
+                glVertex2f(-0.25, -1);
+                glVertex2f(1.25, -1);
+
+                glColor3f(1, 1, 1);
+                glVertex2f(0.5, 0);
             glEnd();
 
             glPopMatrix();
